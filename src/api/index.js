d0b33ece84cf47cddf962fbfd089632a6b81667b
Anton Shpylenia <anton.shpylenia@sixt.com>
2016-10-20 00:32:21 +0300

    postres queries, rest api

--- src/api/index.js
+++ src/api/index.js
@@ -8,7 +8,7 @@ const pgurl = `postgres://iskeizttxypnli:epJ6T3YOyI5dl2zNcjV0_hE8o7@ec2-54-247-1
 const client = new pg.Client(pgurl);
 client.connect();
 
-
+const tableName = 'users';
 
 export default ({config, db}) => {
     let api = Router();
@@ -18,33 +18,51 @@ export default ({config, db}) => {
 
     // perhaps expose some API metadata at the root
     api.get('/', (req, res) => {
-        const response = [];
-        pg.defaults.ssl = true;
-        pg.connect(pgurl, function (err, client) {
-            if (err) throw err;
-            console.log('Connected to postgres! Getting schemas...');
-
-
-            client
-                .query(`SELECT table_schema,table_name
-            FROM information_schema.tables
-            ORDER BY table_schema,table_name;
-            `, [], function (err, result) {
-                    if (err) throw err;
-
-                    // just print the result to the console
-                    console.log(result); // outputs: { name: 'brianc' }
-                    res.json({result});
-                    // disconnect the client
-                    client.end(function (err) {
-                        if (err) throw err;
-                    });
-
-                });
-
+        client.query(`SELECT * FROM ${tableName} ORDER  BY id ASC`, [], function (err, result) {
+            console.log();
+            res.json(result.rows);
+        });
+    }).get('/:id', (req, res) => {
+        const id = req.params.id;
+        client.query(`SELECT * FROM ${tableName} WHERE id = ${id}`, [], function (err, result) {
+            if (result.rows[0]) {
+                res.json(result.rows[0]);
+            } else {
+                res.status(404).send(`No entry with id ${id}`);
+            }
 
         });
-    });
+    }).post('/', (req, res) => {
+        const {name, age} = req.body;
+        client.query(`INSERT INTO ${tableName}(name,age) values($1, $2)`, [name, age], function (err, result) {
+            if (err) res.send(err);
+            res.send(result);
+
+        });
+    }).put('/:id', (req, res) => {
+        const id = req.params.id;
+        client.query(`SELECT * FROM ${tableName} WHERE id = $1`, [id], function (err, result) {
+
+            if (result && result.rows[0]) {
+                Object.keys(req.body).forEach(key => {
+                    client.query(`UPDATE ${tableName} SET ${key} = ${req.body[key]} WHERE id = ${id}`, [], function (err, result) {
+                        console.log(`UPDATE ${tableName} SET ${key} = ${req.body[key]} WHERE id = ${id}`)
+                    });
+                });
+                res.send(`Entry with id ${id} was updated`);
+            } else {
+                res.status(404).send(`No entry with id ${id}`);
+            }
+
+        });
+    }).delete('/:id', (req, res) => {
+        const id = req.params.id;
+        client.query(`DELETE FROM ${tableName} WHERE id = $1`, [id], function (err, result) {
+            console.log(id);
+            res.json(result);
+        });
+    })
+    ;
 
     return api;
 }
--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ app.use(cors({
 app.use(bodyParser.json({
 	limit : config.bodyLimit
 }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // connect to db
 initializeDb( db => {
